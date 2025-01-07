e2699d5cdad5a599de800e8c14e85507f0c9c554
Philemon <victorphilemon77@gmail.com>
2025-01-07 20:17:57 +0300

    Update

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, render_template, request, redirect, url_for, current_app
+import os
+from flask import Blueprint, render_template, request, redirect, url_for
 from app import db
 from app.models import Project, Certification
 
@@ -6,8 +7,28 @@ from app.models import Project, Certification
 bp = Blueprint('main', __name__)
 
 @bp.route('/')
-def index():
+def home():
     return render_template('index.html')
+
+
+@bp.route('/about')
+def about():
+    return render_template('about.html')
+
+@bp.route('/projects')
+def projects():
+    return render_template('projects.html')
+
+@bp.route('/certifications')
+def certifications():
+    return render_template('certifications.html')
+
+@bp.route('/blog')
+def blog():
+    return render_template('blog.html')
+
+
+
 
 @bp.route('/upload_project', methods=['GET', 'POST'])
 def upload_project():
@@ -19,7 +40,7 @@ def upload_project():
         new_project = Project(name=name, description=description, technologies=technologies, status=status)
         db.session.add(new_project)
         db.session.commit()
-        return redirect(url_for('main.index'))
+        return redirect(url_for('main.home'))
     return render_template('upload_project.html')
 
 @bp.route('/upload_certification', methods=['GET', 'POST'])
@@ -28,10 +49,34 @@ def upload_certification():
         title = request.form['title']
         issued_by = request.form['issued_by']
         file = request.files['file']
-        file_path = f"static/certifications/{file.filename}"
+
+        # Define the path where you want to save the uploaded files
+        cert_dir = os.path.join('static', 'certifications')
+
+        # Check if the directory exists, and create it if it doesn't
+        if not os.path.exists(cert_dir):
+            os.makedirs(cert_dir)
+
+        file_path = os.path.join(cert_dir, file.filename)
+
+        # Save the file to the specified directory
         file.save(file_path)
+
+        # Save certification information to the database
         new_cert = Certification(title=title, issued_by=issued_by, file_path=file_path)
         db.session.add(new_cert)
         db.session.commit()
-        return redirect(url_for('main.index'))
+
+        return redirect(url_for('main.home'))
     return render_template('upload_certification.html')
+
+@bp.route('/upload_resume', methods=['GET', 'POST'])
+def upload_resume():
+    if request.method == 'POST':
+        file = request.files['resume-file']
+        file_path = f"static/resumes/{file.filename}"
+        file.save(file_path)
+        # Save resume information to the database, if needed
+        return redirect(url_for('main.home'))
+    return render_template('upload_certification.html')  # You can reuse the same template for both
+

45687b2e44c90b72ff00c65684c4df3792bcf5d5
Philemon <victorphilemon77@gmail.com>
2025-01-08 12:15:47 +0300

    Commit all changes

--- app/static/js/typewriter.js
+++ app/static/js/typewriter.js
@@ -1,15 +1,30 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const textElement = document.querySelector(".typewriter");
-    const text = "Welcome to My Portfolio";
-    let index = 0;
-
-    function typeEffect() {
-        if (index < text.length) {
-            textElement.textContent += text.charAt(index);
-            index++;
-            setTimeout(typeEffect, 100); // Adjust speed as needed
+// typewriter.js
+function typeWriter(elementId, text, speed, callback = null) {
+    const element = document.getElementById(elementId);
+    let i = 0;
+    function type() {
+        if (i < text.length) {
+            element.textContent += text.charAt(i);
+            i++;
+            setTimeout(type, speed);
+        } else if (callback) {
+            callback(); // Trigger the callback after typing completes
         }
     }
+    type();
+}
 
-    typeEffect();
+document.addEventListener('DOMContentLoaded', () => {
+    typeWriter(
+        "introTypewriter",
+        "Hi, I am Philemon Victor, Software Developer & Engineer ...",
+        100,
+        () => {
+            typeWriter("welcomeTypewriter", "Welcome to My Portfolio", 100, () => {
+                if (typeof startCounting === "function") {
+                    startCounting(); // Trigger counting when typing finishes
+                }
+            });
+        }
+    );
 });
